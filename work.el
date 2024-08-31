4c4fede87dad077f5769e4571715fa9489105f92
Dave Townsend <dave@brightspot.com>
2024-08-31 19:49:13 -0400

    Functions moved out of .emacs to here

--- work.el
+++ work.el
@@ -61,4 +61,86 @@ on the previous line if the now-current line starts with a
           (previous-line)
           (replace-regexp-in-region ",[[:space:]]*$" "" (point) (pos-eol))))))
 
+(defun bsp-format-hbs-log ()
+  "Format the output of HBS `{{log}}' statement on the current line.
+
+Useful when analyzing logged objects, whose attributes are otherwise all
+mashed together, which can be difficult to read."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (unless (looking-at "{")
+      (error "current line doesn't look like an HBS log line"))
+    (forward-char)
+    (insert "\n  ")
+    (let ((indent-level 2))
+      (while (> indent-level 0)
+        (cond
+         ((looking-at "=")
+          (insert " ")
+          (forward-char)
+          (insert " "))
+         ((looking-at ", ")
+          (forward-char)
+          (delete-char 1)
+          (insert "\n" (make-string indent-level ? )))
+         ((looking-at "{")
+          (forward-char)
+          (insert "\n")
+          (setq indent-level (+ indent-level 2))
+          (insert (make-string indent-level ? )))
+         ((looking-at "}, ")
+          (setq indent-level (- indent-level 2))
+          (insert "\n" (make-string indent-level ? ))
+          (search-forward "}," nil t)
+          (forward-char)
+          (insert "\n" (make-string indent-level ? )))
+         ((looking-at "}$")
+          (setq indent-level (- indent-level 2))
+          (insert "\n" (make-string indent-level ? ))
+          (search-forward "}" nil t))
+         (t
+          (forward-char)))))))
+
+;;; http://irreal.org/blog/?p=354
+;(defun json-format-region ()
+;  (interactive)
+;  (save-excursion
+;    (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))
+
+(defun bsp-decode-enhancement (rgn-start rgn-end)
+  "Convert RTE enhancement HTML string in region to something more readable.
+
+Requires function `json-format-region` (example commented out above in the source file)
+which in turn requires Python."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region rgn-start rgn-end)
+      ; Unencode quotes
+      (goto-char (point-min))
+      (while (search-forward "&quot;" (point-max) t)
+        (replace-match "\""))
+      (goto-char (point-min))
+      (set-mark (1- (search-forward "{")))
+      (goto-char (point-max))
+      (search-backward "}")
+      (forward-char)
+      (json-format-region))))
+
+(define-key global-map "\C-co" 'bsp-styleguide-other-file)
+(define-key global-map "\C-cy" 'bsp-styleguide-other-type)
+(define-key global-map "\C-ci" 'bsp-styleguide-goto-include)
+(define-key global-map "\C-ct" 'bsp-start-task)
+(define-key global-map "\C-cj"
+            (lambda ()
+              (interactive)
+              (insert "|-----+-------------+------+-----+----+------+-------|\n")
+              (insert "| BUG | DESCRIPTION | DEV? | PR? | QA | PROD | NOTES |\n")
+              (insert "|-----+-------------+------+-----+----+------+-------|\n")
+              (insert "| \n")
+              (insert "|-----+-------------+------+-----+----+------+-------|\n")))
+
+(require 'bsp-styleguide)
+
 (provide 'bsp-work)
